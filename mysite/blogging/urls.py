fd0da9a56cbb1ec5b82aeacde1f463fc3d239e9a
Aime <aime.ayers@outlook.com>
2025-02-22 17:59:53 -0800

    rearrange files, get tests and project working

--- mysite/blogging/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from blogging.views import stub_view
-
-urlpatterns = [
-    path("", stub_view, name="blog_index"),
-]
--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "polling",
     "blogging",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
